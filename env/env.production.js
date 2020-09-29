e036039943f2cea6bea5c009e3b4ae0a0119f618
KaitoTakase <48192611+kaitoooo@users.noreply.github.com>
2020-09-29 10:22:03 +0900

    first commit

---
+++ env/env.production.js
@@ -0,0 +1,4 @@
+export const env = {
+  url: "http://hubers.asia/[本番用ディレクトリ]",
+  base: "/[本番用ディレクトリ]/"
+};
---
+++ env/env.staging.js
@@ -0,0 +1,4 @@
+export const env = {
+  url: "http://hubers.asia/kaito/pro/[本番用ディレクトリ]",
+  base: "/kaito/pro/[本番用ディレクトリ]/"
+};
